d9ef00dc2b4eb4d1e6aa2c3896ef985471e0e667
/rdndev <rdninc@mail.ru>
2023-11-05 23:47:58 +0200

    fix one bug

--- react/examples/src/use-state-bad-example/Example4.jsx
+++ react/examples/src/use-state-bad-example/Example4.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 // bad use of React.useState
 const Example4 = () => {
-    const [task, setTask] = React.useState("");
+    const [tasks, setTasks] = React.useState([]);
 
     const onClickAdd = () => {
         const text = prompt("Enter a task");
-        task.push(text);
-        // good: setTask([...task, text]);
+        tasks.push(text);
+        // good: setTask([...tasks, text]);
     }
 
     return (
         <div>
             <div>
-                {task.map((task, i) => (<p key={i}>{task}</p>))}
+                {tasks.map((task, i) => (<p key={i}>{task}</p>))}
             </div>
             <button onClick={onClickAdd}>Add Task</button>
         </div>
